a7b3f3f6a668d5d5d22b76a676750ce7716c16e3
Nycolas Felipe <nycolaslourenco.contato@gmail.com>
2024-01-15 23:02:21 -0300

    Criação página de publicações

---
+++ client/src/pages/01-home/script.js
@@ -0,0 +1,7 @@
+$('.post-images').slick({
+  arrows: true,
+  infinite: true,
+  lazyLoad: true,
+  prevArrow:"<button type='button' class='slick-prev pull-left btn h-100'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+  nextArrow:"<button type='button' class='slick-next pull-right btn h-100'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+});
--- src/app.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const app = express();
-
-app.get('/', (req, res) => {
-  res.status(200).send("home.html");
-});
-
-export default app;
